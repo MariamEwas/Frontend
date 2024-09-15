78f0d4634242cd479f393ab06b62688cb580ada7
MariamEwas <150853335+MariamEwas@users.noreply.github.com>
2024-09-15 16:48:08 +0300

    Update projects.service.ts

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -10,7 +10,7 @@ export class ProjectsService {
   constructor(private http:HttpClient) {
    }
   
-   apiURL = 'https://gem-ablaze-echo.glitch.me/projects';
+   apiURL = 'https://gem-ablaze-echo.glitch.me/project';
 
   getProjects() :Observable <any>{
     return this.http.get<any>(this.apiURL);
